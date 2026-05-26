224698cf34f8081bb0a996821279fa51a1bac875
tom-marv <tommarv98.tm@gmail.com>
2026-05-27 00:14:03 +0200

    Change target SDK

--- SplitReceipt/app/build.gradle.kts
+++ SplitReceipt/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.tommarv.splitreceipt"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
